e82190cc65267a2fc2d525ba96003f9b521c9d98
mixabyt <150931435+mixabyt@users.noreply.github.com>
2024-06-01 21:15:37 +0300

    Update message_processing.py

--- message_processing.py
+++ message_processing.py
@@ -16,12 +16,12 @@ def tiktok_link(message):
 def press_button(message):
     if check_audio (message) == True:
         kb = types.InlineKeyboardMarkup(row_width=1)
-        audio = types.InlineKeyboardButton(text="Надсилати аудіо ✅", callback_data="audio-")
+        audio = types.InlineKeyboardButton(text="Надсилати аудіо ✅ ", callback_data="audio-")
         kb.add(audio)
         bot.send_message(message.chat.id, text="Налаштуванння чату:", reply_markup=kb)
     else:
         kb = types.InlineKeyboardMarkup(row_width=1)
-        audio = types.InlineKeyboardButton(text="Надсилати аудіо ❌", callback_data="audio+")
+        audio = types.InlineKeyboardButton(text="Надсилати аудіо ❌ ", callback_data="audio+")
         kb.add(audio)
         bot.send_message(message.chat.id, text="Налаштуванння чату:", reply_markup=kb)
 
@@ -29,18 +29,15 @@ def press_button(message):
 def turn_audio(callback):
     if callback.data == 'audio-':
         kb = types.InlineKeyboardMarkup(row_width=1)
-        audio = types.InlineKeyboardButton(text="Надсилати аудіо ❌", callback_data="audio+")
+        audio = types.InlineKeyboardButton(text="Надсилати аудіо ❌ ", callback_data="audio+")
         kb.add(audio)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text="Налаштуванння чату:", reply_markup=kb)
         turn_audio_false(callback)
 
-    
+
     if callback.data == 'audio+':
         kb = types.InlineKeyboardMarkup(row_width=1)
-        audio = types.InlineKeyboardButton(text="Надсилати аудіо ✅", callback_data="audio-")
+        audio = types.InlineKeyboardButton(text="Надсилати аудіо ✅ ", callback_data="audio-")
         kb.add(audio)
         bot.edit_message_text(chat_id=callback.message.chat.id, message_id=callback.message.id, text="Налаштуванння чату:", reply_markup=kb)
         turn_audio_true(callback)
-
-
-    
